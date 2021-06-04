92bddde622f90cc601adf04240947555e7ece021
akbaridria <akbaridria15@gmail.com>
2021-06-05 01:44:06 +0700

    done web desktop

---
+++ src/components/Contact.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="contact">
+        <center>
+        <h3 style="margin-bottom : 20px">Hi, feel free to contact or follow me on my social media below!</h3>
+        <a href="https://twitter.com/a_idria" target="_blank"><i class="fab fa-twitter fa-2x" style="color: white;"></i></a>
+        <a href="https://www.linkedin.com/in/akbaridria/" target="_blank"><i class="fab fa-linkedin fa-2x" style="color: white;"></i></a>
+        <a href="https://github.com/akbaridria" target="_blank"><i class="fab fa-github fa-2x" style="color: white;"></i></a>
+        </center>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.contact {
+    color : white;
+    margin : 200px auto;
+}
+
+i {
+    margin : 0 20px;
+}
+</style>
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -6,37 +6,140 @@
         <div class="cards">
             <div class="card">
                 <div class="image" style="margin-right : 20px">
-                    <img :src="require('../assets/AvatarMaker.png')" width="100" />
+                    <i class="fas fa-desktop fa-2x" style="color: white;"></i>
                 </div>
                 <div class="textImage">
+                    <h3>One Of The Winners in 'OneMillionWallets' Hackathon</h3>
                     <h4>Project Name</h4>
-                    <p>Etherman</p>
+                    <p>CryptoWalletIDR</p>
                     <h4>Purpose</h4>
-                    <p>Web apps that can check matic and ethereum wallet address</p>
+                    <p>Web apps that can check balance, transaction and assets from their ethereum wallet address</p>
                     <h4>Tools and Requirements</h4>
-                    <p>Vue Js, Covalent API</p>
+                    <p>HTML, CSS, Javascript , Covalent API</p>
+                    <h4>Github Repo</h4>
+                    <p><a href="https://github.com/akbaridria/cryptoWalletIDR" target="_blank">CryptoWalletIDR repo</a></p>
+                    <h4>Annocuncement Link</h4>
+                    <p><a href="https://www.covalenthq.com/blog/omw-winners-announcement/" target="_blank">click here</a></p>
                 </div>
             </div>
             <hr />
             <div class="card">
                 <div class="image" style="margin-right : 20px">
-                    <img :src="require('../assets/AvatarMaker.png')" width="100" />
+                    <i class="fas fa-mobile-alt fa-2x" style="color: white;"></i>
                 </div>
                 <div class="textImage">
+                    <h3>One of the winners in DnD Aave V2 Hackathon category dApps</h3>
+                    <h4>Project Name</h4>
+                    <p>CAAVE</p>
+                    <h4>Purpose</h4>
+                    <p>Mobile Apps that can tract governance proposal and network assets in AAVE.</p>
+                    <h4>Tools and Requirements</h4>
+                    <p>ReactNative, Covalent API</p>
+                    <h4>Github Repo</h4>
+                    <p><a href="https://github.com/akbaridria/CAAVE" target="_blank">CAAVE repo</a></p>
+                    <h4>Annocuncement Link</h4>
+                    <p><a href="https://www.covalenthq.com/blog/dnd-aave-winners-announcement/" target="_blank">click here</a></p>
+                </div>
+            </div>
+            <hr />
+            <div class="card">
+                <div class="image" style="margin-right : 20px">
+                    <i class="fas fa-desktop fa-2x" style="color: white;"></i>
+                </div>
+                <div class="textImage">
+                    <h3>One of the winners in 'OneMillionWallets - Avalanche' Hackathon</h3>
+                    <h4>Project Name</h4>
+                    <p>AWC</p>
+                    <h4>Purpose</h4>
+                    <p>Web Apps that can check the balance of avalance wallet, their transaction, portofolio, and also their nfts</p>
+                    <h4>Tools and Requirements</h4>
+                    <p>ReactNative, Covalent API</p>
+                    <h4>Github Repo</h4>
+                    <p><a href="https://github.com/akbaridria/AWC" target="_blank">AWC repo</a></p>
+                    <h4>Annocuncement Link</h4>
+                    <p><a href="https://www.covalenthq.com/blog/omw-avalanche-winners-announced/" target="_blank">click here</a></p>
+                </div>
+            </div>
+            <hr />
+            <div class="card">
+                <div class="image" style="margin-right : 20px">
+                    <i class="fas fa-desktop fa-2x" style="color: white;"></i>
+                </div>
+                <div class="textImage">
+                    <h3>One of the winners in ‘Etherpunk 2021’ Hackathon</h3>
                     <h4>Project Name</h4>
                     <p>Etherman</p>
                     <h4>Purpose</h4>
-                    <p>web apps that can check matic and ethereum wallet address</p>
+                    <p>On Etherman, you can find all assets in the wallet like their balances, transaction or NFT, and Uniswap assets, Sushiswap assets, or spot price in various currencies</p>
                     <h4>Tools and Requirements</h4>
                     <p>Vue Js, Covalent API</p>
+                    <h4>Github Repo</h4>
+                    <p><a href="https://github.com/akbaridria/EtherMan" target="_blank">Etherman repo</a></p>
+                    <h4>Annocuncement Link</h4>
+                    <p><a href="https://devfolio.co/blog/meet-the-etherpunks/" target="_blank">click here</a></p>
                 </div>
             </div>
+            <hr />
+            <div class="card">
+                <div class="image" style="margin-right : 20px">
+                    <i class="fas fa-desktop fa-2x" style="color: white;"></i>
+                </div>
+                <div class="textImage">
+                    <h4>Project Name</h4>
+                    <p>C-One</p>
+                    <h4>Purpose</h4>
+                    <p>Dashboard that can track swap transaction and chi gas token in 1inch v3</p>
+                    <h4>Tools and Requirements</h4>
+                    <p>Vue Js, Covalent API</p>
+                    <h4>Github Repo</h4>
+                    <p><a href="https://github.com/akbaridria/c-one" target="_blank">Etherman repo</a></p>
+                    <h4>Demo Link</h4>
+                    <p><a href="https://dnd1inch.akbaridria.com/" target="_blank">click here</a></p>
+                </div>
+            </div>
+            <hr />
+            <div class="card">
+                <div class="image" style="margin-right : 20px">
+                     <i class="fad fa-chart-bar fa-2x" style="color: white;"></i>
+                </div>
+                <div class="textImage">
+                    <h4>Project Name</h4>
+                    <p>Compare Aave Protocol V1 vs V2</p>
+                    <h4>Purpose</h4>
+                    <p>Dashboard to campare AAVE V1 and AAVE V2 </p>
+                    <h4>Tools and Requirements</h4>
+                    <p>Google data Studio, Python, Covalent API</p>
+                    <h4>Demo Link</h4>
+                    <p><a href="https://datastudio.google.com/u/0/reporting/10835502-63e2-4725-9af0-10184c6475e0/page/n2L8B" target="_blank">click here</a></p>
+                </div>
+            </div>
+            <hr />
+            <div class="card">
+                <div class="image" style="margin-right : 20px">
+                    <i class="fas fa-robot fa-2x" style="color: white;"></i>
+                </div>
+                <div class="textImage">
+                    <h4>Project Name</h4>
+                    <p>BigSwapCovalent</p>
+                    <h4>Type</h4>
+                    <p>BOT</p>
+                    <h4>Purpose</h4>
+                    <p>Tract The last trancsaction in uniswap and sushiswap and then make a tweet about it.</p>
+                    <h4>Tools and Requirements</h4>
+                    <p>Python, Covalent API, Twitter API</p>
+                    <h4>Demo Link</h4>
+                    <p><a href="https://twitter.com/BigSwapCovalent" target="_blank">click here</a></p>
+                </div>
+            </div>
+            <hr />
+            <h3>And still there's more.. you can check it on my github</h3>
         </div>
     </div>
 </template>
 <script>
+
 export default {
-    name : 'Work'
+    name : 'Work',
 }
 </script>
 <style scoped>
@@ -45,16 +148,33 @@ export default {
     color : white;
 }
 
+a {
+    text-decoration: none;
+    color : white;
+}
 .card {
     display: flex;
     flex-direction: row;
     align-items: center;
     line-height: 1.2;
-    margin:0;
-    padding:0;
+    margin : 30px 0;
 }
-
-h4 {
+.image {
+    width : 50px;
+}
+.cards {
+    margin-top : 50px;
+    display : flex;
+    flex-direction : column;
+    justify-content: center;
+}
+a:hover, h4 {
     color : #0d6efd;
+}
+p{
+    margin-bottom : 2px;
+}
+h3 {
+    margin-bottom: 10px;
 }
 </style>
